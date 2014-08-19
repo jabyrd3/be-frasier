1ab342ca9bebf120641b3a0217b3b37e34a2fb06
Jordan <jabyrd3@gmail.com>
2014-08-19 11:16:58 -0400

    WHOO FRASIER UPDATES

--- jerk.js
+++ jerk.js
@@ -4,7 +4,7 @@ var options = {
     server: 'irc.slashnet.org',
     nick  : 'insufferable',
     channels : ['#mefi-frasier', '#mefi']
-}
+};
 jerk(function(j){
     j.watch_for('be frasier', function(message){
         message.say(say());    
